3ce97d13961bc8fcf24f29bf2c422eec8643e274
carbroz26 <carbroz67@gmail.com>
2026-09-23 02:33:25 +0530

    feat: enable Kotlin Compose compiler convention

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     implementation(gradleKotlinDsl())
     implementation("com.android.tools.build:gradle:${libs.versions.agp.get()}")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:${libs.versions.kotlin.get()}")
+    implementation("org.jetbrains.kotlin:compose-compiler-gradle-plugin:${libs.versions.kotlin.get()}")
     implementation("org.jetbrains.compose:compose-gradle-plugin:${libs.versions.composeMultiplatform.get()}")
 }
 
