bc0cfa85198abac8cda3adc524fafe055085267a
carbroz26 <carbroz67@gmail.com>
2026-09-23 01:36:13 +0530

    feat: configure Android application compile sdk

--- build-logic/src/main/kotlin/com/carbroz/cbpartner/buildlogic/AndroidApplicationConventionPlugin.kt
+++ build-logic/src/main/kotlin/com/carbroz/cbpartner/buildlogic/AndroidApplicationConventionPlugin.kt
@@ -1,10 +1,16 @@
 package com.carbroz.cbpartner.buildlogic
 
+import com.android.build.api.dsl.ApplicationExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
 
 class AndroidApplicationConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         target.pluginManager.apply("com.android.application")
+
+        target.extensions.configure<ApplicationExtension> {
+            compileSdk = 36
+        }
     }
 }
