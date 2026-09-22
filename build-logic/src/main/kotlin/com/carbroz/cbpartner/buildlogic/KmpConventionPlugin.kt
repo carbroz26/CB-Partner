9a4273e6060e57bf9a7d3b62c0bab55c5fba50cf
carbroz26 <carbroz67@gmail.com>
2026-09-22 21:59:50 +0530

    feat: configure KMP shared targets

--- build-logic/src/main/kotlin/com/carbroz/cbpartner/buildlogic/KmpConventionPlugin.kt
+++ build-logic/src/main/kotlin/com/carbroz/cbpartner/buildlogic/KmpConventionPlugin.kt
@@ -2,9 +2,16 @@ package com.carbroz.cbpartner.buildlogic
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 
 class KmpConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         target.pluginManager.apply("org.jetbrains.kotlin.multiplatform")
+
+        target.extensions.configure<KotlinMultiplatformExtension> {
+            iosArm64()
+            iosSimulatorArm64()
+            jvm()
+        }
     }
 }
