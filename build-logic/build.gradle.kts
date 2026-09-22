110793b918f56f84f0e2ac6c290edf2af974cac3
carbroz26 <carbroz67@gmail.com>
2026-09-23 02:07:02 +0530

    fix: expose Gradle Kotlin DSL to build logic

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
 }
 
 dependencies {
+    implementation(gradleKotlinDsl())
     implementation("com.android.tools.build:gradle:${libs.versions.agp.get()}")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:${libs.versions.kotlin.get()}")
     implementation("org.jetbrains.compose:compose-gradle-plugin:${libs.versions.composeMultiplatform.get()}")
