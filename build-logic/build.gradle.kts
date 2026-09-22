cac3979cf9c7c6e9d9c3f764f618456bf65b7dfd
carbroz26 <carbroz67@gmail.com>
2026-09-22 20:39:25 +0530

    feat: add build logic convention plugin foundation

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -1,1 +1,37 @@
-// Convention plugins are introduced in BASE-ARCH-014 unit 014-03.
+plugins {
+    `java-gradle-plugin`
+    kotlin("jvm") version "2.4.20"
+}
+
+repositories {
+    google()
+    mavenCentral()
+    gradlePluginPortal()
+}
+
+dependencies {
+    implementation("com.android.tools.build:gradle:9.3.0")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.20")
+    implementation("org.jetbrains.compose:compose-gradle-plugin:1.12.0")
+}
+
+gradlePlugin {
+    plugins {
+        register("cbPartnerKmp") {
+            id = "cbpartner.kmp"
+            implementationClass = "com.carbroz.cbpartner.buildlogic.KmpConventionPlugin"
+        }
+        register("cbPartnerCompose") {
+            id = "cbpartner.compose"
+            implementationClass = "com.carbroz.cbpartner.buildlogic.ComposeConventionPlugin"
+        }
+        register("cbPartnerAndroidApplication") {
+            id = "cbpartner.android.application"
+            implementationClass = "com.carbroz.cbpartner.buildlogic.AndroidApplicationConventionPlugin"
+        }
+        register("cbPartnerDesktopApplication") {
+            id = "cbpartner.desktop.application"
+            implementationClass = "com.carbroz.cbpartner.buildlogic.DesktopApplicationConventionPlugin"
+        }
+    }
+}
