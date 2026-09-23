ac6d4c67c7052483ec66ad49843cd7be654c1954
carbroz26 <carbroz67@gmail.com>
2026-09-23 07:52:54 +0530

    feat: wire foundation module dependencies

--- feature/splash/build.gradle.kts
+++ feature/splash/build.gradle.kts
@@ -2,3 +2,13 @@ plugins {
     id("cbpartner.kmp")
     id("cbpartner.compose")
 }
+
+kotlin {
+    sourceSets {
+        commonMain.dependencies {
+            implementation(project(":domain"))
+            implementation(project(":core"))
+            implementation(project(":navigation"))
+        }
+    }
+}
